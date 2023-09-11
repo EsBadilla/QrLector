1d444c04a773a6db328477ea661c7c6ec3ca9e83
dilon badilla <es.badilla@duocuc.cl>
2023-09-11 18:20:41 -0300

    Agregando una animación

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,18 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { AnimationController } from '@ionic/angular';
+import { Animation, AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-dato:any;
-  constructor(private route: ActivatedRoute) {
+export class HomePage implements OnInit{
+  private animacion: Animation = {} as Animation;
+  dato:any;
+  constructor(private route: ActivatedRoute, private AnimationCtrl: AnimationController) {
     this.route.paramMap.subscribe((params)=>{
       this.dato = params.get('data');
     })
   }
+  ngOnInit() {
+    this.animacion = this.AnimationCtrl.create()
+    .addElement(document.querySelectorAll(".texto"))
+    .duration(2000)
+    .iterations(Infinity)
+    .fromTo('transform', 'translateX(-300px)', 'translateX(300px)')
+    this.animacion.play();
 
+  }
 }
+function ngOnInit() {
+  throw new Error('Function not implemented.');
+}
+
