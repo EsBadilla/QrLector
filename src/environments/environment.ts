5479800eeaee8cd87a28c80b60fbb2caa69f8550
Plaza Vespucio <es.badilla@duocuc.cl>
2023-10-10 11:54:28 -0300

    Si

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCTHJlGzEesU4AI13EjSCAnlmbxJEGM1gw",
+    authDomain: "registrapp-3c85e.firebaseapp.com",
+    projectId: "registrapp-3c85e",
+    storageBucket: "registrapp-3c85e.appspot.com",
+    messagingSenderId: "678694296592",
+    appId: "1:678694296592:web:9cd1c7611091fb7fabbe95",
+    measurementId: "G-ND8VMDGBJP"
+  }
 };
 
 /*
